4b2676a2ddafaafa9ab7737e24f43f85aaac6310
Stanislav <45733236+AdiStas@users.noreply.github.com>
2019-11-09 01:35:14 +0300

    tablet версия готова до .price

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// открытие/закрытие меню
+
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
@@ -12,3 +14,21 @@ navToggle.addEventListener('click', function() {
 		navMain.classList.remove('main-nav--opened');
 	}
 });
+
+
+// nojs
+
+// var navMain = document.querySelector('.main-nav');
+// var navToggle = document.querySelector('main-nav__toggle');
+
+// navMain.classList.remove('main-nav--nojs');
+
+// navToggle.addEventListener('click', function() {
+// 	if (navMain.classList.contains('main-nav--closed')) {
+// 		navMain.classList.remove('main-nav--closed');
+// 		navMain.classList.add('main-nav--opened');
+// 	} else {
+// 		navMain.classList.add('main-nav--closed');
+// 		navMain.classList.remove('main-nav--opened');
+// 	}
+// });
